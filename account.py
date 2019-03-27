0d1de609f6f40430390425040485387c8a502233
ZhitingChia <35999032+ZhitingChia@users.noreply.github.com>
2019-03-27 14:27:33 +0800

    Add files via upload

---
+++ account.py
@@ -0,0 +1,97 @@
+#This program creates user logins and then
+#allows them to loginto the system
+#the system will display the time they loggeed in too
+
+#Author: Chia Zhi Ting
+#Version 1.0
+#Created on: 27/3/2019
+#Stores password as plaintext
+
+import time
+
+users={}
+option=''
+
+def mainMenu():
+    global option
+    option=input("Do you have a login account? (y/n/q)")
+    if option=='y':
+        login()
+    elif option =='n':
+        createUser()
+    elif option =='q':
+        print("Proceeding to quit.")
+    else:
+        print("Invalid input! Please enter y/n/q")
+
+def run():
+    global option
+    while option != 'q':
+        mainMenu()
+    print ("Successfully quit.")
+
+def createUser():
+    uname=input("Enter a new login name: ")
+    upass = input ("Enter new password: ")
+    
+    if(validateCreation(uname,upass)):
+        writeAccount(uname,upass)
+        print("Account {} successfully created!".format(uname))
+    else:
+        createUser()
+
+   
+          
+
+def validateCreation(lname,lpass):
+    if lname in users:
+        print("Username {} already exists!".format(lname))
+        return False
+    else:
+        return True
+
+def writeAccount(lname,lpass):
+    outfile= open("ac.txt",'a')
+    outfile.write("{}:{}".format(lname,lpass))
+    outfile.write("\n")
+    outfile.close()
+
+    
+def login():
+    readAccount()
+    uname= input("Enter login name: ")
+    upass=input("Enter password: ")
+
+    if(validateLogin(uname,upass)):
+        print("User {} logged in successfully at {}".format(uname,time.strftime("%H:%M")))
+    else:
+        option=input("Do you wish to quit? (y/n)")
+        if option == 'y':
+            option ='q'
+        else:
+            login()
+        
+
+def validateLogin(lname,lpass):
+    if lname not in users:
+        print("User {} does not exist!".format(lname))
+        return False
+    elif users[lname]==lpass:
+        return True
+    else:
+        print("Wrong password!")
+
+
+def readAccount():
+    infile=open("ac.txt")
+
+    for line in infile:
+        lname,lpass= line.split(':')
+        lpass=lpass.strip('\n')
+        users[lname]=lpass
+        
+    infile.close()
+
+    
+    
+run()
